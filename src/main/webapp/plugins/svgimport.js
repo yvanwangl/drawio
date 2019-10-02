53845f622fe6b57251a547028ad20bb5e67083c2
wangyafei07 <wangyafei07@meituan.com>
2019-10-02 10:37:20 +0800

    modify importsvg

--- src/main/webapp/plugins/svgimport.js
+++ src/main/webapp/plugins/svgimport.js
@@ -5,7 +5,7 @@ Draw.loadPlugin(function (ui) {
 
   // Adds resource for action
   // mxResources.parse('exportSvg=Export Svg...');
-  window.importSvg = (svgData) => {
+  window.importSvg = ({ svgData }) => {
     let page = ui.createPage();
     ui.pages.push(page);
     ui.selectPage(page, true);
