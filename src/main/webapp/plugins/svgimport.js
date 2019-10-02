f57235d00bbe82a4748e24b0ebb6dc604df490bb
wangyafei07 <wangyafei07@meituan.com>
2019-10-02 12:00:50 +0800

    import svg file

--- src/main/webapp/plugins/svgimport.js
+++ src/main/webapp/plugins/svgimport.js
@@ -2,6 +2,18 @@
  * Sample plugin.
  */
 Draw.loadPlugin(function (ui) {
+  let graph = ui.editor.graph;
+  var doImportFile = mxUtils.bind(this, function (data, mime, filename) {
+    // Gets insert location
+    var view = graph.view;
+    var bds = graph.getGraphBounds();
+    var x = graph.snap(Math.ceil(Math.max(0, bds.x / view.scale - view.translate.x) + 4 * graph.gridSize));
+    var y = graph.snap(Math.ceil(Math.max(0, (bds.y + bds.height) / view.scale - view.translate.y) + 4 * graph.gridSize));
+    ui.importFile(data, mime, x, y, 0, 0, filename, function (cells) {
+      graph.setSelectionCells(cells);
+      graph.scrollCellToVisible(graph.getSelectionCell());
+    });
+  });
 
   // Adds resource for action
   // mxResources.parse('exportSvg=Export Svg...');
@@ -9,6 +21,10 @@ Draw.loadPlugin(function (ui) {
     let page = ui.createPage();
     ui.pages.push(page);
     ui.selectPage(page, true);
-    ui.importXml(svgData);
+    const div = document.createElement('div');
+    div.innerHTML = svgData;
+    let file = new LocalFile(ui, div.innerHTML, 'canary.svg', true);
+    file.setData(ui.createSvgDataUri(file.getData()));
+    doImportFile(file.getData(), 'image/svg+xml', file.getTitle());
   };
 });
